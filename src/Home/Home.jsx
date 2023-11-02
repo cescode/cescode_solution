20772b67021e2f151ca34660249662dbb64c3caf
leruizr <leruizres1986mail.com>
2023-11-01 19:13:28 -0500

    se crea la base del proyecto

---
+++ src/Home/Home.jsx
@@ -0,0 +1,16 @@
+import './Home.css'
+import { Menu } from '../Menu/Menu'
+export function Home() {
+    return (
+        <>
+            <header>
+                <Menu></Menu>
+            </header>
+
+            <section className='banner'>
+                <p>SOLUCION RETO EQUIPO CESCODE</p>
+            </section>
+            
+        </>
+    )
+}
